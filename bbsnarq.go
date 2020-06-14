8eb41b6c944819ee03bdf9913edbaa48654fe309
John Doe <johndoe@example.com>
2020-06-14 22:00:47 +0100

    try post to api

--- bbsnarq.go
+++ bbsnarq.go
@@ -3,16 +3,27 @@ package main
 import (
     "fmt"
     "os"
+    "net/http"
 )
 
 func main() {
+    url := "http://bbsstore-service:7002/api/dns_store"
+    fmt.Println("URL:>", url)
 
-    argsWithProg := os.Args
-    argsWithoutProg := os.Args[1:]
+    var jsonStr = []byte(`{"domain":"`+os.Args[2]+`"}`)
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+    req.Header.Set("X-Custom-Header", "myvalue")
+    req.Header.Set("Content-Type", "application/json")
 
-    arg := os.Args[3]
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
 
-    fmt.Println(argsWithProg)
-    fmt.Println(argsWithoutProg)
-    fmt.Println(arg)
+    fmt.Println("response Status:", resp.Status)
+    fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
 }
