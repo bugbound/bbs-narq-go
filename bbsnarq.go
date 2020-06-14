826d401d0e0c32046652ce07443238b54a476149
John Doe <johndoe@example.com>
2020-06-14 22:38:39 +0100

    pipe

--- bbsnarq.go
+++ bbsnarq.go
@@ -10,7 +10,15 @@ import (
     "io/ioutil"
 )
 
+
 func main() {
+    s := bufio.NewScanner(os.Stdin)
+    for s.Scan() {
+        log.Println("line", s.Text())
+    }
+}
+
+func mainOLD() {
 	// check if there is somethinig to read on STDIN
 	stat, _ := os.Stdin.Stat()
 	if (stat.Mode() & os.ModeCharDevice) == 0 {
@@ -49,26 +57,4 @@ func main() {
 	} else {
 		fmt.Println("send data via pipe")
     }
-}
-
-func mainOLD() {
-    url := "http://bbsstore-service:7002/api/dns_store"
-    fmt.Println("URL:>", url)
-
-    var jsonStr = []byte(`{"domain":"`+os.Args[2]+`"}`)
-    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-    req.Header.Set("X-Custom-Header", "myvalue")
-    req.Header.Set("Content-Type", "application/json")
-
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
-
-    fmt.Println("response Status:", resp.Status)
-    fmt.Println("response Headers:", resp.Header)
-    body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Println("response Body:", string(body))
 }
