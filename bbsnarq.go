4c1f69867cdbe2984677a1bb070a04eb99761217
John Doe <johndoe@example.com>
2020-06-14 23:13:50 +0100

    pipe

--- bbsnarq.go
+++ bbsnarq.go
@@ -16,8 +16,38 @@ func main() {
     for s.Scan() {
         var linetext = s.Text()
         log.Println("line", linetext)
-        adddomain(linetext)
+        
+        if os.Args[1] == "domain" {
+            adddomain(linetext)
+        }
+        if os.Args[1] == "url" {
+            addurl(linetext)
+        }        
     }
+}
+
+
+func addurl(domain string) {
+    url := "http://bbsstore-service:7002/api/url_store"
+    var jsonStrStart = []byte(`{"url":"`)
+    var jsonStrEnd = []byte(`"}`)
+    var part1 = append(jsonStrStart, domain...)
+    var completeValue = append(part1, jsonStrEnd...)
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(completeValue))
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    //fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
+    
 }
 
 func adddomain(domain string) {
