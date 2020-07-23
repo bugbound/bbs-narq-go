48e5564b8fba67fb27298a083544f1380431ecc1
John Doe <johndoe@example.com>
2020-07-23 08:08:51 +0100

    added ip

--- bbsnarq.go
+++ bbsnarq.go
@@ -22,6 +22,9 @@ func main() {
         }
         if os.Args[1] == "url" {
             addurl(linetext)
+        } 
+        if os.Args[1] == "ip" {
+            addIP(linetext, os.Args[2])
         }        
     }
 }
@@ -49,6 +52,36 @@ func addurl(domain string) {
     fmt.Println("response Body:", string(body))
     
 }
+
+func addIP(ip string, domain string) {
+    url := "http://bbsstore-service:7002/api/ip_store"
+    var jsonStrStart = []byte(`{"ip":"`)
+    var jsonStrNext = []byte(`", "domain":"`)
+    var jsonStrEnd = []byte(`"}`)
+    var part1 = append(jsonStrStart, ip...)
+    var part2 = append(part1, jsonStrNext...)
+    var part3 = append(part2, domain...)
+    var completeValue= append(part3, jsonStrEnd...)
+    
+    
+    
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(completeValue))
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    //fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
+    
+}
+
 
 func adddomain(domain string) {
     url := "http://bbsstore-service:7002/api/dns_store"
